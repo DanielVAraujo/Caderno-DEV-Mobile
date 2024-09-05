24186a3c7179e7b3af9fa6144bd973ced4f1b371
DanielVAraujo <160507946+DanielVAraujo@users.noreply.github.com>
2024-09-05 11:11:18 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,13 @@
+fun main() {
+  /*
+    Programa Teste de Funções em Kotlin
+    Programador: Daniel Araújo
+
+  */
+  println ("Olá Mundo!")
+  //println (3 + 9)
+  println (5*7)
+  var frase="Uma Frase do Tipo String"
+  //frase="Mudei a Frase"
+  println(frase)
+}
